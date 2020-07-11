483becf1065dd68800dcb08b9f89857692c525d9
mohammadsalek <salek.mohmd@gmail.com>
2020-07-12 03:30:34 +0430

    few minor improvements

--- spacin/loader.py
+++ spacin/loader.py
@@ -1,5 +1,21 @@
+import os
+from os import listdir
+from os.path import isfile, join
+
+
 class Loader:
     """Load data from files into usable data"""
+
+    def __init__(self):
+        self.abs_data_dir = os.path.abspath("./data")
+
+    def get_db_wordlists(self):
+        """Return dict of wordlist names + abs path in data dir"""
+        return {
+            file: join(self.abs_data_dir, file)
+            for file in listdir(self.abs_data_dir)
+            if isfile(join(self.abs_data_dir, file))
+        }
 
     @staticmethod
     def load_file(filepath):
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,19 +2,22 @@ from spacin.loader import Loader
 
 import unittest
 from os import listdir
-from os.path import isfile, join, abspath
 
 
 class TestLoader(unittest.TestCase):
-    """Test to load data from files into usable data"""
+    """Test Loader class"""
 
     def setUp(self):
-        """Set up class"""
         self.data_dir = "./data"
 
     def test_files_exist_in_data_dir(self):
         """Test data dir is not empty"""
+        loader = Loader()
+        wordlist_dict = loader.get_db_wordlists()
+
         self.assertTrue(listdir(self.data_dir))
+        self.assertIsNotNone(wordlist_dict)
+        self.assertEqual(type(wordlist_dict), dict)
 
     def test_load_file_file_not_exists(self):
         """Test given filepath exists"""
@@ -22,3 +25,5 @@ class TestLoader(unittest.TestCase):
 
         with self.assertRaises(FileNotFoundError):
             Loader().load_file(filapath)
+
+        filapath = "NOT a file path"
