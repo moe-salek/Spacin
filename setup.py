71f189f337c55def1a1dba249b0b21fbfcca4a73
mohammadsalek <salek.mohmd@gmail.com>
2020-07-14 17:22:16 +0430

    fixed style

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='Spacin',
+    name='spacin',
     version='0.1',
-    description='Puts space between words',
-    author='MohammadSalek',
+    description='puts space in!',
+    author='Mohammad Salek',
+    url='https://gitlab.com/mohammadsalek/spacin',
     packages=['spacin'],
     entry_points={'console_scripts': ['spacin = spacin.__main__:main']},
 )
