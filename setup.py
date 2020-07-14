0172949f33d68fe34f1a7ccd85dc86a6b8ffee3d
mohammadsalek <salek.mohmd@gmail.com>
2020-07-14 17:23:00 +0430

    changed setup

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='spacin',
-    version='0.1',
     description='puts space in!',
+    version='0.1',
     author='Mohammad Salek',
     url='https://gitlab.com/mohammadsalek/spacin',
     packages=['spacin'],
