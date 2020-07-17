23083ddff17db427831fd6d792ff5dae0a408b3c
mohammadsalek <salek.mohmd@gmail.com>
2020-07-17 13:16:19 +0430

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='spacin',
-    version='0.1.0',
-    description='Spacin, puts space between!',
-    long_description='Spacin is a word-separator that distinguishes each word in a given string.',
+    name="spacin",
+    version="0.1.0",
+    description="Spacin, puts space between!",
+    long_description="Spacin is a word-separator that distinguishes each word in a given string.",
     long_description_content_type="text/markdown",
-    author='Mohammad Salek',
-    url='https://gitlab.com/mohammadsalek/spacin',
+    author="Mohammad Salek",
+    url="https://gitlab.com/mohammadsalek/spacin",
     license="MIT",
     include_package_data=True,
-    packages=['spacin', 'data'],
-    entry_points={'console_scripts': ['spacin = spacin.__main__:main']},
+    packages=["spacin", "data"],
+    entry_points={"console_scripts": ["spacin = spacin.__main__:main"]},
 )
