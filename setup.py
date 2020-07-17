9f836a7f499350f126440b8398185875308b9c13
mohammadsalek <salek.mohmd@gmail.com>
2020-07-17 12:05:58 +0430

    minro changes

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
     name='spacin',
+    version='0.0.0',
     description='puts space in!',
-    version='0.1',
     author='Mohammad Salek',
     url='https://gitlab.com/mohammadsalek/spacin',
+    license="MIT",
+    include_package_data=True,
     packages=['spacin', 'data'],
     entry_points={'console_scripts': ['spacin = spacin.__main__:main']},
 )
