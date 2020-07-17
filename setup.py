98382d604391b68bd9bf3dd50094e948966f5088
mohammadsalek <salek.mohmd@gmail.com>
2020-07-17 13:37:53 +0430

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
+
+LONG_DESC = "Spacin is a word-separator that \
+ distinguishes each word in a given string."
+
 setup(
     name="spacin",
     version="0.1.0",
     description="Spacin, puts space between!",
-    long_description="Spacin is a word-separator \
-    that distinguishes each word in a given string.",
-    long_description_content_type="text/markdown",
+    long_description=LONG_DESC,
+    long_description_content_type="text/plain",
     author="Mohammad Salek",
     url="https://gitlab.com/mohammadsalek/spacin",
     license="MIT",
